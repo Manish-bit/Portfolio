708a1338fd966660c2bc1ee6e3c3cb3d1dd994e1
Manish Aryal <manish.aryal@dzangolab.com>
2024-06-03 22:16:22 +0545

    feat: add image for profile

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,9 +7,9 @@ const Home = () => {
   return (
     <div className="bg-[#0a192f] w-full h-screen" name="home">
       {/* Container */}
-      <div className="max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full">
-        <div className="w-[300px] h-[300px] mx-auto">
-          <img src={Profile} alt="error" className="w-[100%] rounded-[20rem]" />
+      <div className="max-w-[1000px] m-auto px-8 flex flex-col justify-center h-full">
+        <div className="w-[200px] h-[100px] mx-auto aspect-auto">
+          <img src={Profile} alt="error" className="w-[100%] rounded-[50%]" />
         </div>
         <div className="mt-[10rem]">
           <p className="text-pink-600">Hi,my name is </p>
